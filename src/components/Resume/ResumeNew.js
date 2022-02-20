3d327876061e9000882fae9fda07f762aaaf81f3
Ayon <mirelahi33@gmail.com>
2022-02-20 02:13:21 -0500

    updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -32,6 +32,7 @@ function ResumeNew() {
         <Row className="resume">
           <Document file={pdf}>
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
