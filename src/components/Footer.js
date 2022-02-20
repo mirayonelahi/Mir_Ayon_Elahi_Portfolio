f785f002780e8febf230570ee275e85f8049286a
Ayon <mirelahi33@gmail.com>
2022-02-19 21:12:25 -0500

    ss added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,16 +14,16 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Soumyajit Behera</h3>
+          <h3>Mir Ayon Elahi</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} Mir Ayon Elahi</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/mirayonelahi"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/Soumyajit4419"
+                href="https://twitter.com/elahimirayon"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
